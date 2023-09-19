b9f3abfa42bf17ece23e75528ca9b5d3a9bdc553
RalfKellner <ralf.kellner@uni-passau.de>
2023-09-19 11:58:06 +0200

    updated course book

---
+++ my_module.py
@@ -0,0 +1,5 @@
+def print_python_is_fun():
+    print("Python is fun to learn!")
+
+def add_two(number):
+    return number + 2
---
+++ my_package/your_module.py
@@ -0,0 +1,5 @@
+def print_something_else():
+    print("Something else")
+
+def add_three(number):
+    return number + 3
